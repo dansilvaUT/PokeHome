3e26a693167d2f9e099a96bf3dfa132089e8db7d
dansilvaUT <dan13silv@gmail.com>
2023-06-15 15:08:11 -0600

    Update some methods and add update user profile image method.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::UsersController < ApplicationController
+    include UserModule
+
     def create
         @user = User.new(user_params)
         if @user.save
@@ -9,11 +11,20 @@ class Api::UsersController < ApplicationController
         end
     end
 
-    # TODO: Add edit/update methods
+    def update_user_profile_img
+        if current_user
+            @user = current_user
+            if @user.update(profile_img: user_params[:image])
+                render json: { message: "User profile image updated" }
+            else
+                render json: { error: @user.errors }, status: :unprocessable_entity
+            end
+        end
+    end
 
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :password, :image)
     end
 end
